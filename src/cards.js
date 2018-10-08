9c560f95b805ead044603672a7966e7d974cd8a1
phoebe li <phoebe.li@dynamotechnologies.com>
2018-10-07 23:59:35 -0400

    implement function to shuffle deck

--- src/cards.js
+++ src/cards.js
@@ -1,4 +1,4 @@
-import shuffle from 'shuffle-array';
+// import shuffle from 'shuffle-array';
 
 const suits = ['C', 'D', 'H', 'S'];
 const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
@@ -9,17 +9,36 @@ const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
  * @param {string} value - The value of the card
  * @param {string,number} suit - The suite of the card
  */
-export const card = (value, suit) => ({
+const card = (value, suit) => ({
   face: value + suit,
   value,
   suit
 });
 
 /**
- * Function for generating a shuffled deck of cards. The shuffle method uses Fisher-Yates algorithm
+ * Shuffle a deck of cards.  Loop through the array and swap the card with a random one in the same deck.
+ *
+ * @param {array} cards - A deck of cards
+ */
+
+const shuffle = (cards) => {
+  for (let i = 0; i < cards.length; i++) {
+    let idx = Math.floor(Math.random() * 52);
+    [cards[i], cards[idx]] = [cards[idx], cards[i]];
+  }
+  return cards;
+}
+
+/**
+ * Generator function for a shuffled deck of cards.
  *
  */
+
 export const generateDeck = () =>
     shuffle(suits.reduce((cards, suit) =>
         [...cards, ...values.map(value => card(value, suit))]
     , []));
+
+
+
+
